8af5ac2a538934fffc8fec4f47ab8e0056be1827
Steve Jones <jonesst2608@gmail.com>
2022-08-27 15:10:14 +0100

    Fix error

--- tests/multipage_test.py
+++ tests/multipage_test.py
@@ -6,5 +6,5 @@ def test_page_nav1(duo, test_app):
 
     # Confirm we're on page 1
 
-    result = duo.wait_for_text_to_equal(".display-4", "Welcome to XXX DashSPA", timeout=20)
+    result = duo.wait_for_text_to_equal(".display-4", "Welcome to DashSPA", timeout=20)
     assert result
